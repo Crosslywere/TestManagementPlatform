8b3b01c83521cff652691f606af5c5c93a8ccfea
Crosslywere <ogboruj@outlook.com>
2025-12-22 13:18:03 +0100

    Project setup

---
+++ src/main/java/com/crosslywere/tmp/TestManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.crosslywere.tmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crosslywere/tmp/TestManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosslywere.tmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
