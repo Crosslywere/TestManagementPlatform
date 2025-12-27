0bef3793979c30907798c257e0cfbb4951c027bc
Crosslywere <ogboruj@outlook.com>
2025-12-27 12:37:17 +0100

    Optimised imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,11 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ClassroomTable, CreateClassroom } from "./component/Classroom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/create"
-          element={
-            <div className="container my-5">
-              <CreateClassroom />
-            </div>
-          }
-        />
-        <Route
-          path="/table"
-          element={
-            <div className="container my-5">
-              <ClassroomTable />
-            </div>
-          }
-        />
+        <Route />
       </Routes>
     </BrowserRouter>
   );
